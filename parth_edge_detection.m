d7b8865b41bc3df54d292444e312f5de6ca489b8
Parth Shukla <119152160+Parthshukla26@users.noreply.github.com>
2025-01-16 15:18:34 +0530

    Update color_to_grey_scale_parth.m

--- parth_edge_detection.m
+++ parth_edge_detection.m
@@ -1,16 +1,17 @@
-% Created on 15/01/25
-% Created by Parth Shukla., BT22ECE079
-% Modified to add additional functionalities
+% Created on 11/01/25
+% Created by Parth Shukla, BT22ECE079
+% First Practical to convert Color image to Grayscale.
+
 
 clc
 clear all
 close all
 
 % Reading the image
-I = imread("landscape.jpg");
+I = imread('image1.jpg');
 
 % Displaying value of a specified pixel
-disp(I(1010,505));
+disp(I(400,600));
 
 % Grayscale img using single layer of color img
 I_gray1 = I(:,:,1);
@@ -19,38 +20,37 @@ I_gray1 = I(:,:,1);
 I_r = I(:,:,1);
 I_g = I(:,:,2);
 I_b = I(:,:,3);
-I_gray2 = round((I_r + I_g + I_b) / 3);
+I_gray2 = round((I_r+I_g+I_b)/3);                           
 
 % Grayscale img using luminosity method
-I_gray3 = round(0.299 * I_r + 0.587 * I_g + 0.114 * I_b);
+I_gray3 = round(0.299*I_r+0.587*I_g+0.114*I_b);
 
-% Binary image using thresholding
-threshold = 128; % Set threshold value
-I_binary = imbinarize(I_gray3, threshold / 255);
+% Red version of image
+Ired = I;
+Ired(:,:,2) = 0;
+Ired(:,:,3) = 0;
 
-% Image rotation
-rotation_angle = 45; % Rotation angle in degrees
-I_rotated = imrotate(I, rotation_angle, 'bilinear', 'crop');
+% Green version of image
+Igreen = I;
+Igreen(:,:,1) = 0;
+Igreen(:,:,3) = 0;
 
-% Edge detection using Canny method
-I_edges = edge(I_gray3, 'canny');
+% Blue version of image
+Iblue = I;
+Iblue(:,:,1) = 0;
+Iblue(:,:,2) = 0;
 
-% Displaying the original, grayscale, and binary images
+% Displaying RGB versions
 figure(1)
-subplot(2, 2, 1), imshow(I); xlabel("Original Image");
-subplot(2, 2, 2), imshow(I_gray1); xlabel("Grayscale Image #1");
-subplot(2, 2, 3), imshow(I_gray2); xlabel("Grayscale Image #2");
-subplot(2, 2, 4), imshow(I_binary); xlabel("Binary Image");
+subplot (2,2,1), imshow(I); xlabel('Reference Image');
+subplot (2,2,2), imshow(I_gray1); xlabel('Grayscale Image #1');
+subplot (2,2,3), imshow(I_gray2); xlabel('Grayscale Image #2');
+subplot (2,2,4), imshow(I_gray3); xlabel('Grayscale Image #3');
 
-% Displaying additional functionalities: rotation and edge detection
+
+% Displaying Grayscale versions using various methods
 figure(2)
-subplot(1, 3, 1), imshow(I); xlabel("Original Image");
-subplot(1, 3, 2), imshow(I_rotated); xlabel("Rotated Image");
-subplot(1, 3, 3), imshow(I_edges); xlabel("Edge Detection (Canny)");
-
-% Saving results as images
-imwrite(I_binary, "BinaryImage.jpg");
-imwrite(I_rotated, "RotatedImage.jpg");
-imwrite(I_edges, "EdgeDetection.jpg");
-
-disp("Image processing complete. Outputs saved as images.");
+subplot (2,2,1), imshow(I); xlabel('Reference Image');
+subplot (2,2,2), imshow(Ired); xlabel('Red Image');
+subplot (2,2,3), imshow(Igreen); xlabel('Green Image');
+subplot (2,2,4), imshow(Iblue); xlabel('Blue Image');
